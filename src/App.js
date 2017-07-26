a067589f17813820ce9cb7716ddbfe0ca3ee00fe
Julia Mitchelmore <julia@foundersfactory.co>
2017-07-26 21:31:09 +0100

    remove app garbage

--- src/App.js
+++ src/App.js
@@ -17,16 +17,10 @@ class App extends Component {
       <Provider firebaseApp={ firebaseApp }>
         <div className="App">
           <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-
             <CreatePlayer/>
             <PrepareGame/>
 
           </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
         </div>
       </Provider>
     );
