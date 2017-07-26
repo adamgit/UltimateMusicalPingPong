db6413efaf1deaadb701e7d9fe710bb21d923b89
Julia Mitchelmore <julia@foundersfactory.co>
2017-07-26 21:29:44 +0100

    create a player and select from dropdowns

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Provider } from 'react-firebase'
 import { initializeApp } from 'firebase'
 import logo from './logo.svg';
+import CreatePlayer from './CreatePlayer.js';
+import PrepareGame from './PrepareGame.js';
 import './App.css';
 
 const firebaseApp = initializeApp({
@@ -9,6 +11,7 @@ const firebaseApp = initializeApp({
 })
 
 class App extends Component {
+
   render() {
     return (
       <Provider firebaseApp={ firebaseApp }>
@@ -16,6 +19,10 @@ class App extends Component {
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h2>Welcome to React</h2>
+
+            <CreatePlayer/>
+            <PrepareGame/>
+
           </div>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
@@ -26,4 +33,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
