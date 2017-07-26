4aa98e9dd3be183329e336452de2cac416d03071
miteshsevani <msevani@gmail.com>
2017-07-26 21:30:44 +0100

    Created score card

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,12 @@ import React from "react";
 
 export default class Button extends React.Component {
     render() {
+
+        const {onclick} = this.props;
+        
         return (
             <div>
-                <button>Start</button>
+                <button onClick={onclick}>Start</button>
             </div>
         )
     }
