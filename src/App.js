34497d5a39c2e5e50395ac82ff078c709b03f576
Julia Mitchelmore <julia@foundersfactory.co>
2017-07-26 20:28:38 +0100

    should add firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-firebase'
+import { initializeApp } from 'firebase'
 import logo from './logo.svg';
 import './App.css';
+
+const firebaseApp = initializeApp({
+  databaseURL: 'https://ultimatemusicalpingpong.firebaseio.com/'
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Provider firebaseApp={ firebaseApp }>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Provider>
     );
   }
 }
